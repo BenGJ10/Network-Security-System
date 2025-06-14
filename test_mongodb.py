97b32a7a7b8870a1697588b2278c58d1a83a19c1
Ben Gregory John <140584616+BenGJ10@users.noreply.github.com>
2025-06-14 12:25:03 +0530

    Minor updations

--- test_mongodb.py
+++ test_mongodb.py
@@ -3,10 +3,10 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 # Replace <username> and <password> with your actual username and password
-uri = "mongodb+srv://<username>:<password>@cluster0.sfppbhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+url = "mongodb+srv://<username>:<password>@cluster0.sfppbhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'), tls = True, tlsCAFile=certifi.where())
+client = MongoClient(url, server_api=ServerApi('1'), tls = True, tlsCAFile=certifi.where())
 
 # Send a ping to confirm a successful connection
 try:
